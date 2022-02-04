ad0c515a86af88e10eb97423779ad95801c6c44a
mochi256 <contact@mochi256.com>
2022-02-04 13:59:03 +0900

    update: add comment

--- constants.py
+++ constants.py
@@ -1,10 +1,19 @@
+# number of genomes to generate
 GENERATE_GENOMS = 250
 
+# genoms to leave for the next generation
 ELITE_GENOMS = int(GENERATE_GENOMS * 0.2)
 PROGENY_GENOMS = int(GENERATE_GENOMS * 0.4)
 NORMAL_GENOMS = GENERATE_GENOMS - (ELITE_GENOMS + PROGENY_GENOMS)
 
+# maximum number of genes
 GENE_MAX = 100
+
+# probability of mutation(1/n)
 GENE_MUTATION = 100
+
+# probability of crossover(1/n)
 GENOM_CROSSOVER = 100
+
+# number of generations to try
 GENOM_GENERATION = 100
--- run.py
+++ run.py
@@ -10,12 +10,18 @@ from constants import (
 )
 
 if __name__=='__main__':
+    # generate genoms
     current_genoms = [Genom() for idx in range(GENERATE_GENOMS)]
+
+    # function to be executed when sorting genomes.
     extact_genom_score = lambda g: g.get_score()
 
     for loop in range(GENOM_GENERATION):
         next_genoms = []
+
+        # sort the genome by score.
         current_genoms = sorted(current_genoms, key=extact_genom_score, reverse=True)
+        # show score
         print("{0} generasion score:".format(loop + 1))
         print("\thigh: {0}".format(current_genoms[0].get_score()))
         print("\tmedian: {0}".format(
@@ -26,10 +32,12 @@ if __name__=='__main__':
         ))
         print("")
 
+        # extracting superior genomes
         elites = [
             current_genoms[idx] for idx in range(ELITE_GENOMS)
         ]
 
+        # genomes create offspring from each other.
         progenies = []
         for idx in range(PROGENY_GENOMS):
             g1 = random.randint(0, GENERATE_GENOMS - 1)
@@ -38,6 +46,7 @@ if __name__=='__main__':
                 current_genoms[g1].crossover(current_genoms[g2])
             )
         
+        # leave any genome to the next generation
         normals = []
         for idx in range(NORMAL_GENOMS):
             pickup_genom_idx = random.randint(0, (GENERATE_GENOMS - (idx + 1)))
@@ -45,18 +54,23 @@ if __name__=='__main__':
                 current_genoms.pop(pickup_genom_idx)
             )
         
+        # generating the next generation of genomes
         next_genoms.extend(elites)
         next_genoms.extend(progenies)
         next_genoms.extend(normals)
+
         if len(next_genoms) != GENERATE_GENOMS:
             raise Exception()
         
+        # mutate in all genomes with arbitrary probability
         for genom in next_genoms:
             genom.mutation()
         
+        # update "current_genoms" and execute the next generation process.
         current_genoms = copy.deepcopy(next_genoms)
         del next_genoms
     
+    # show the last score
     current_genoms = sorted(current_genoms, key=extact_genom_score, reverse=True)
     print("last generasion score: {0}".format(current_genoms[0].get_score()))
     print("\thigh: {0}".format(current_genoms[0].get_score()))
